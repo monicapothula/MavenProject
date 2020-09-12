1dae91ecf42333cfa676ba8ee4d835eddbc38805
Pothula <monica.pothula@teradata.com>
2020-09-12 17:21:09 +0530

    start project

---
+++ src/main/java/com/mp/MyProject/App.java
@@ -0,0 +1,14 @@
+package com.mp.MyProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is our first project " );
+        System.out.println("this is new line");
+    }
+}
